3bbd8fc22df1c97b4b712f46bc5ddf033e2c30ea
Jwhyee <kawnsdud@naver.com>
2022-06-22 17:33:58 +0900

    [BOJ] / 문자열 / 브론즈

--- ka-jun-young/LikeLionStudy/src/algorithm/BackJoon/For_10952.java
+++ ka-jun-young/LikeLionStudy/src/algorithm/BackJoon/For_10952.java
@@ -7,7 +7,6 @@ public class For_10952 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-//        /ㅁㄴㅇㄹㅁㄴㅇㄹ
         while(true){
             StringTokenizer st = new StringTokenizer(br.readLine(), " ");
             int a = Integer.parseInt(st.nextToken());
