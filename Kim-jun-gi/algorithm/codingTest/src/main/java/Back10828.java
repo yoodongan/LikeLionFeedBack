308824edfb4b8f20b9ae4136fd053c0bd13da850
wnsrl1228 <wnsrl981228@gmail.com>
2022-06-25 00:51:55 +0900

    [BOJ] / 문자열 / 브2
    파일 재생성

--- Kim-jun-gi/algorithm/codingTest/src/main/java/Back10828.java
+++ Kim-jun-gi/algorithm/codingTest/src/main/java/Back10828.java
@@ -1,8 +1,7 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
-
-public class Main {
+public class Back10828 {
     public static int[] stack;
     public static int size = 0;
     public static void main(String[] args) throws Exception{
