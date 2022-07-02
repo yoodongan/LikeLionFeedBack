98ca1a9c53ff52ef63af2ee024a204b28d33b340
Jwhyee <kawnsdud@naver.com>
2022-07-02 23:57:11 +0900

    [PRG] 오픈채팅방 30분

--- ka-jun-young/LikeLionStudy/src/main/java/algorithm/programmers/OpenChatting.java
+++ ka-jun-young/LikeLionStudy/src/main/java/algorithm/programmers/OpenChatting.java
@@ -4,26 +4,29 @@ import java.util.*;
 
 public class OpenChatting {
     public static String[] solution(String[] record) {
-        String[] answer = {};
         Map<String, String> userMap = new HashMap<>();
         List<String> statusList = new ArrayList<>();
-        StringTokenizer st;
-        String userId = "";
-        String userNickname = "";
-        String userStatus = "";
 
         for (String info : record) {
-            st = new StringTokenizer(info);
-            userStatus = st.nextToken();
-            if (userStatus.equals("Enter")) {
-                userId = st.nextToken();
-                userNickname = st.nextToken();
-                if (!userMap.containsKey(userId)) {
-                    userMap.put(userId, userNickname);
-                    statusList.add(userNickname);
-                }
+            String[] userInfo = info.split(" ");    // 0 : Enter 1 : uid1234 2: Muzi
+            if (userInfo[0].equals("Enter")) {
+                statusList.add(userInfo[1] + "님 Enter");
+                userMap.put(userInfo[1], userInfo[2]);
+            } else if (userInfo[0].equals("Change")) {
+                userMap.put(userInfo[1], userInfo[2]);
+            } else {
+                statusList.add(userInfo[1] + "님 Leave");
             }
+        }
 
+        String[] answer = new String[statusList.size()];
+
+        for (int i = 0; i < statusList.size(); i++) {
+            if (statusList.get(i).contains("Enter")) {
+                answer[i] = userMap.get(statusList.get(i).replaceAll("님 Enter", "")) + "님이 들어왔습니다.";
+            } else {
+                answer[i] = userMap.get(statusList.get(i).replaceAll("님 Leave", "")) + "님이 나갔습니다.";
+            }
         }
         return answer;
     }
