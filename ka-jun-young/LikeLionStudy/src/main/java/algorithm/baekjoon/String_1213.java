9a3ec78ca45eb7438e53e0f8bab8eff813c4a2ba
Jwhyee <kawnsdud@naver.com>
2022-07-05 15:48:20 +0900

    [BOJ] / 팰린드롬 만들기 / 실버3

--- ka-jun-young/LikeLionStudy/src/main/java/algorithm/baekjoon/String_1213.java
+++ ka-jun-young/LikeLionStudy/src/main/java/algorithm/baekjoon/String_1213.java
@@ -6,7 +6,7 @@ import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.Stack;
 
-public class Main {
+public class String_1213 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         // Test case 입력 받기
